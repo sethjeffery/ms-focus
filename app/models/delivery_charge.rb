ea2f83c0c34a6da0bf27799518b08975d20c5516
Seth Jeffery <seth@quipper.com>
2017-09-12 10:59:07 +0100

    ensure DeliveryCharge.price_for_total is tested

--- app/models/delivery_charge.rb
+++ app/models/delivery_charge.rb
@@ -1,5 +1,5 @@
 class DeliveryCharge < ApplicationRecord
   def self.price_for_total(total)
-    where("price_threshold > ?", total).first.try(:price).to_i
+    where("price_threshold > ?", total).first&.charge.to_i
   end
 end
--- spec/models/delivery_charge_spec.rb
+++ spec/models/delivery_charge_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe DeliveryCharge, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '.price_for_total' do
+    it 'returns the delivery charge price for the given total' do
+      create(:delivery_charge, price_threshold: 100, charge: 149)
+      create(:delivery_charge, price_threshold: 200, charge: 249)
+
+      # below threshold
+      expect(described_class.price_for_total(99)).to eq 149
+
+      # above threshold
+      expect(described_class.price_for_total(100)).to eq 249
+    end
+
+    context 'with no charges above the total' do
+      it 'is free' do
+        create(:delivery_charge, price_threshold: 100, charge: 100)
+        expect(described_class.price_for_total(200)).to eq 0
+      end
+    end
+  end
 end
