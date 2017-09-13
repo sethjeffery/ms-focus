814997a402ced32a5852fc22fcf78d301aafa784
Seth Jeffery <seth@quipper.com>
2017-09-13 20:51:56 +0100

    add CORS allowance for frontend app

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,17 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # CORS
+  config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins 'localhost:3001'
+
+      resource '*',
+               headers: :any,
+               methods: [:get, :post, :put, :patch, :delete, :options, :head],
+               expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+               max_age: 0
+      end
+  end
 end
